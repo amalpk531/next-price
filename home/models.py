fb38bcb151dd2ea11ada014dfaf65e3a5e79a204
amalpk531 <amal18120007@gmail.com>
2025-01-11 23:25:01 +0530

    dashboard included

--- home/models.py
+++ home/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Contact(models.Model):
-    name=models.CharField(max_length=30)
+    name=models.CharField(max_length=30,null=False, blank=False)
     email=models.EmailField()
     description=models.TextField()
     def __str__(self) :
